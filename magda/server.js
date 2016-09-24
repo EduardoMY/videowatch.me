6638a7b0a14041d184dc58f7ba8ad3e9363d1d0e
Antonio de Luna Lopez <tonydelun@gmail.com>
2016-09-24 01:04:08 -0700

    agregar y hacerle queries a un video de un usuario

--- magda/server.js
+++ magda/server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var sqlite3 = require('sqlite3').verbose();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var videos = {};
 
 app.get('/upload', function(req, res) {
@@ -16,6 +22,30 @@ app.get('/lobby', function (req, res) {
 });
 
 app.get('/videos', function (req, res) {
+
+})
+
+app.post('/:userId/upload', function(req, res) {
+  db = new sqlite3.Database("video.db")
+  db.serialize(function() {
+  db.run('insert into videos(usuario_fk, titulo, video_link, tumbnail) values(' + 
+      req.params["userId"] + ', "' +
+      req.body["titulo"] + '", "' +
+      req.body["video"] + '", "' +
+      req.body["tumbnail"] + 
+  '")');
+  })
+  db.close();
+});
+
+app.get('/:userId/query', function(req, res) {
+  db = new sqlite3.Database("video.db")
+  db.serialize(function() {
+    db.all('select * from videos where usuario_fk = ' + req.params["userId"], function(err, rows) {
+      res.json(rows);
+    })
+  })
+  db.close();
 
 })
 
