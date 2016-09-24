8a7fe81ea1c52b81b8cd6c78dc69f6b0bcceccee
EdgarMagdaleno <edgarmv97@gmail.com>
2016-09-24 11:53:24 -0700

    algo clean wey

--- magda/create.js
+++
@@ -1,9 +0,0 @@
-var createTorrent = require('create-torrent')
-var fs = require('fs')
- 
-createTorrent(__dirname + 'test.txt', function (err, torrent) {
-  if (!err) {
-    fs.writeFile('my.torrent', torrent);
-  }
-  fs.writeFile('my.torrent', torrent);
-})
--- magda/upload.js
+++
@@ -1,7 +0,0 @@
-var WebTorrent = require('webtorrent')
-var fs = require('fs')
-var client = new WebTorrent()
-
-client.seed(file, function (torrent) {
-  console.log('Client is seeding ' + torrent.magnetURI)
-})
