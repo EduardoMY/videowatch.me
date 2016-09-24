a1cad4fe273b82c0b79764523231dba429dbb16e
eduardomy <eduardo.medina@cetys.edu.mx>
2016-09-24 02:33:33 -0700

    thumbnails now have 3 pictures

--- medina/screenshotTaker.js
+++ medina/screenshotTaker.js
@@ -1,6 +1,29 @@
-var folderPath='/home/emedina/Downloads', file='SampleVideo_1280x720_2mb.mp4';
+//Simple Paths and fileNames
+var folderPath='/home/emedina/Downloads', file='SampleVideo_1280x720_2mb.mp4', ffprobePath='/usr/bin/ffprobe';
+
+//resolution of the thumbnails
+var resolution='1280x720';
+    
+//Set the ffmpeg variable
 var ffmpeg = require('fluent-ffmpeg');
-var command = ffmpeg(folderPath+'/'+file).
+
+getData();
+
+function getData(){
+  ffmpeg.setFfprobePath(ffprobePath);
+    ffmpeg.ffprobe(folderPath+'/'+file, function(err, metadata) {
+    if (err) {
+        console.error(err);
+    } else {
+	makeScreenShots(metadata.format.duration);
+    }
+    });
+
+}
+
+function makeScreenShots(duration){
+    console.log(duration);
+    var command = new ffmpeg(folderPath+'/'+file).
     on('filenames', function(filenames) {
 	console.log('screenshots are ' + filenames.join(', '));
   }).
@@ -11,12 +34,10 @@ var command = ffmpeg(folderPath+'/'+file).
     console.log('an error happened: ' + err.message);
   }).
   // take 2 screenshots at predefined timemarks and size
-    takeScreenshots({ count: 2, timemarks: [ '00:00:02.000', '1' ], size: '150x100' }, folderPath);
-
-
-
-
-
-
-
-
+	takeScreenshots({ count: 3, timemarks: [ duration*.2, duration*.5, duration*.8  ], size: resolution }, folderPath);
+/*
+    console.log(duration*.2);
+    console.log(duration*.5);
+    console.log(duration*.8);
+*/
+}
