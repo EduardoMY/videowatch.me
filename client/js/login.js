1b79fb1ba921454d38b2d7f138f4fc2dc16ba00a
Javier Rizzo <javierrizzoa@gmail.com>
2016-09-24 14:50:16 -0700

    I really should've committed this when I finished it...

--- client/js/login.js
+++ client/js/login.js
@@ -1,3 +1,43 @@
 let request = require('request');
 
-request.post({url: 'https://javierrizzo.com:6969/'}, function(err, resp, body) {console.log(resp)});
+function login(user, pass, callback) {
+  request.post({
+    url:"http://videowatch.me/login",
+    body: {user: user, password: pass},
+    json:true
+  }, function(error, response, body) {
+    callback(body);
+  });
+}
+
+function register(user, pass, callback) {
+  request.post({
+    url:"http://videowatch.me/register",
+    body: {user: user, password: pass},
+    json:true
+  }, function(error, response, body) {
+    if(body.success) {
+      $('#login-success').show();
+      $('#login-failed').hide();
+    }
+    authenticate(callback, false);
+  });
+}
+
+function authenticate(callback, isRegister) {
+  if(isRegister) {
+    register($('#login-user').val(), $('#login-pass').val(), callback);
+  } else {
+    login($('#login-user').val(), $('#login-pass').val(), callback);
+  }
+}
+
+function success(body) {
+  if(body.login) {
+    window.key = body.key;
+    $('#screen-login').hide(600);
+    $('#screen-upload').show(600);
+  } else {
+    $('#login-failed').show();
+  }
+}
