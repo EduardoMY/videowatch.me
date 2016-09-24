3a864774626f66299b5c04823c9b9dc938843871
Antonio de Luna Lopez <tonydelun@gmail.com>
2016-09-24 05:14:29 -0700

    cambiar nombres en la db

--- magda/server.js
+++ magda/server.js
@@ -25,12 +25,35 @@ app.get('/videos', function (req, res) {
 
 })
 
+app.post('/login', function(req, res) {
+  db = new sqlite3.Database("video.db")
+  db.serialize(function() {
+    db.get('select * from users where user = ' + req.body["user"] + ' and password = ' + req.params["password"], function(err, rows) {
+      if(rows) {
+        var num = getRandomInt(1, 100000);
+
+        res.json({login: true, key:num })
+      }
+      else {
+        res.json({login: false})
+      }
+    })
+  })
+  db.close();
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+      
 app.post('/:userId/upload', function(req, res) {
   db = new sqlite3.Database("video.db")
   db.serialize(function() {
-  db.run('insert into videos(usuario_fk, titulo, video_link, tumbnail) values(' + 
+  db.run('insert into videos(user, title, video, tumbnail) values(' + 
       req.params["userId"] + ', "' +
-      req.body["titulo"] + '", "' +
+      req.body["title"] + '", "' +
       req.body["video"] + '", "' +
       req.body["tumbnail"] + 
   '")');
@@ -41,7 +64,7 @@ app.post('/:userId/upload', function(req, res) {
 app.get('/:userId/query', function(req, res) {
   db = new sqlite3.Database("video.db")
   db.serialize(function() {
-    db.all('select * from videos where usuario_fk = ' + req.params["userId"], function(err, rows) {
+    db.all('select * from videos where user = ' + req.params["userId"], function(err, rows) {
       res.json(rows);
     })
   })
